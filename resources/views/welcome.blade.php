8b5783d5332d2619a16457777fe82fa83f6224be
Your Name <you@example.com>
2017-09-23 12:35:18 -0700

    test66

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,7 +77,7 @@
                 </div>
             @endif
 
-            <div class="content">
+            <div class="content"> 
                 <div class="title m-b-md">
                     Laravel
                 </div>
